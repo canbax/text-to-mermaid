2568b503ce3df080d2481663db400dd3449ad2b2
canbax <yusufsaidcanbaz@gmail.com>
2026-01-24 19:51:33 +0300

    feat: generate 2 texts on demo

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { textToMermaid } from "./index.ts";
 
-const input = "User clicks Button";
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
-textToMermaid(input, { useAiFallback: true }).then((result) => {
-  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+async function renderMermaid(input: string) {
+  const result = await textToMermaid(input, { useAiFallback: true });
+  const div = document.createElement("div");
+  div.innerHTML = `
     <div>
       <h3>Input: "${input}"</h3>
       <h3>Output:</h3>
       <pre class="mermaid">${result}</pre>
     </div>
+    <hr/>
   `;
-});
+  app.appendChild(div);
+}
+
+await renderMermaid("User clicks Button");
+await renderMermaid(
+  "The fast multipole method (FMM) is a numerical technique that was developed to speed up the calculation of long-ranged forces in the n-body problem",
+);
